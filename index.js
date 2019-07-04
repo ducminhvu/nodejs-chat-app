4373bcb75c7a83adad0d92b9486d588f4163b22a
Duc Vu <vu.minh.duc@vareal.vn>
2019-07-04 17:58:29 +0700

    add index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+  res.send('Server(socket.io) is runnig!');
+});
+
+io.on('connection', (socket) => {
+  socket.on('client-send-desc', (data) => {
+    io.emit('server-send-desc', data);
+  });
+});
+
+const server = http.listen(8080, function() {
+  console.log('listening on *:8080');
+});
